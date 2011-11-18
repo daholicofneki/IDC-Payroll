c9f63c9ecb7bba76cc8f664faf88496730ebe9c9
purwandi <free6300@gmail.com>
2011-11-18 08:37:24 +0700

    Fix thickbox layout, Load css, js hanya ketika action GET, klo request dilakukan via ajax maka asset tidak perlu diload

--- application/views/layouts/main.php
+++ application/views/layouts/main.php
@@ -10,4 +10,6 @@
 <body>
 <?php echo $content; ?>
 </body>
+<script type="text/javascript" src="<?php echo base_url()?>static/js/thickbox.js"></script>
+<link href='<?php echo base_url()?>static/css/thickbox.css' rel='stylesheet' type='text/css' />
 </html>
